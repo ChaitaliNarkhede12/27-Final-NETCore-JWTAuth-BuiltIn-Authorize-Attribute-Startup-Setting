5d4b78d9ad1072c948b1550820f8b9cbd22f3dae
Chaitali <chaitali_narkhede@epam.com>
2022-07-15 17:50:39 +0530

    check-in

---
+++ NETCore-JWTAuth-BuiltIn-Authorize-Startup-Setting/Application/Servcies/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Servcies
+{
+    public class UserService : IUserService
+    {
+        public UserModel GetUserDetails(LoginModel loginModel)
+        {
+            if (loginModel != null && loginModel.UserName == "admin" && loginModel.Password == "abc")
+            {
+                UserModel user = new UserModel()
+                {
+                    UserId = 1,
+                    Name = "Admin",
+                    Email = "admin@gmail.com",
+                    UserName = "admin",
+                    Password = "abc"
+                };
+
+                return user;
+            }
+            return null;
+        }
+    }
+}
+
